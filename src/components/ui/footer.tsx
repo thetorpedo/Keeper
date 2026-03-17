217205aed6885e4bba835227d5868b3fd6db8ef2
thiago <thiago.szborges@gmail.com>
2026-03-17 13:05:15 -0400

    refactor: added semver on footer

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -1,9 +1,13 @@
 
 function Footer() {
   return (
-        <footer className='bg-black text-white/40 px-10 py-3 text-sm w-full max-md:text-center'>
+        <footer className='bg-black relative text-white/40 px-10 py-3 text-sm w-full max-md:text-center'>
             <p className='text-2xl text-white font-extrabold font-alegraya mb-2'>Keeper</p>
-            <p>© 2026 Keeper is an unofficial, fan-made tool and is not affiliated with, sponsored, or endorsed by The Adventure Guild, LLC.</p>
+            <div className="gap-2 max-sm:flex flex-col">
+                <p className="sm:float-left">© 2026 Keeper is an unofficial, fan-made tool and is not affiliated with, sponsored, or endorsed by The Adventure Guild, LLC.</p>
+                <p className="sm:float-right">v1.0.0</p>
+            </div>
+            
         </footer>
     );
 }
